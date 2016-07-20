c89a52535a9210494970367255171dddd2177774
abdul alim <abdul@41studio.com>
2016-07-20 11:45:13 +0700

    fix s3

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -8,14 +8,4 @@ if Rails.env.production?
     }
     config.fog_directory  = ENV['S3_FOG_DIRECTORY']
   end
-else
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-        :provider               => 'AWS',
-        :aws_access_key_id      => 'S3_ACCESS_KEY_ID',
-        :aws_secret_access_key  => 'S3_SECRET_ACCESS_KEY'
-        # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-    }
-    config.fog_directory  = 'S3_FOG_DIRECTORY'
-  end
 end
